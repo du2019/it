f5e294aa81ca1f02aa4ddb9f1b98a9a5d59f1607
TLR2019 <tianbao_94@163.com>
2019-06-13 17:41:46 +0800

    第一次提交

---
+++ iTrust/httptests/edu/ncsu/csc/itrust/http/CreateHCPTest.java
@@ -0,0 +1,99 @@
+package edu.ncsu.csc.itrust.http;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebForm;
+import com.meterware.httpunit.WebLink;
+import com.meterware.httpunit.WebResponse;
+
+public class CreateHCPTest extends iTrustHTTPTest {
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		gen.clearAllTables();
+		gen.admin1();
+		gen.cptCodes();
+	}
+
+	/*
+	 * Authenticate admin 90000000001
+	 * Choose Add HCP option
+	 * Physican type not currently implemented
+	 * [Role: Licensed Physician]
+	 * [Enabled: true]
+	 * Last name: Williams
+	 * First name: Laurie
+	 * Email: laurie@ncsu.edu
+	 * Street address 1: 900 Main Campus Dr
+	 * Street address 2: BOX 2509
+	 * City: Raleigh
+	 * State: NC
+	 * Zip code: 27606-1234
+	 * Phone: 919-100-1000
+	 */
+	public void testCreateValidHCP() throws Exception {
+		// login admin
+		WebConversation wc = login("9000000001", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		assertEquals("iTrust - Admin Home", wr.getTitle());
+		// click on Add HCP
+		wr = wr.getLinkWith("Add HCP").click();
+		// add the hcp
+		assertEquals("iTrust - Add HCP", wr.getTitle());
+		WebForm form = wr.getForms()[0];
+		form.setParameter("firstName", "Laurie");
+		form.setParameter("lastName", "Williams");
+		form.setParameter("email", "laurie@ncsu.edu");
+		wr = form.submit();
+		// edit the hcp
+		wr = wr.getLinkWith("Continue").click();
+		assertEquals("iTrust - Edit Personnel", wr.getTitle());
+		form = wr.getForms()[0];
+		form.setParameter("streetAddress1", "900 Main Campus Dr");
+		form.setParameter("streetAddress2", "Box 2509");
+		form.setParameter("city", "Raleigh");
+		form.setParameter("state", "NC");
+		form.setParameter("zip1", "27606");
+		form.setParameter("zip2", "1234");
+		form.setParameter("phone1", "919");
+		form.setParameter("phone2", "100");
+		form.setParameter("phone3", "1000");
+		form.getSubmitButtons()[0].click();
+		wr = wc.getCurrentPage();
+		assertTrue(wr.getText().contains("Information Successfully Updated"));
+	}
+	public void testEditHospitalAssignments() throws Exception {
+		gen.clearAllTables();
+		gen.standardData();
+		// login admin
+		WebConversation wc = login("9000000001", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		assertEquals("iTrust - Admin Home", wr.getTitle());
+		// click on Edit HCP Assignment to Hospital
+		wr = wr.getLinkWith("Edit HCP Assignment to Hospital").click();
+		assertEquals("iTrust - Please Select a Personnel", wr.getTitle());
+		wr.getForms()[1].setParameter("FIRST_NAME","Kelly");
+		wr.getForms()[1].setParameter("LAST_NAME","Doctor");
+		wr.getForms()[1].getButtons()[1].click();
+		wr = wc.getCurrentPage();
+		wr.getForms()[2].getButtons()[0].click();
+		wr = wc.getCurrentPage();
+		// assign hospital
+		assertEquals("iTrust - Hospital Staffing Assignments", wr.getTitle());
+		WebLink[] weblinks = wr.getLinks();
+		for(int i = 0; i < weblinks.length; i++) {
+			if(weblinks[i].getText().equals("Assign")) {
+				wr = weblinks[i].click();
+				assertTrue(wr.getText().contains("HCP has been assigned"));
+				break;
+			}
+		}
+		for(int i = 0; i < weblinks.length; i++) {
+			if(weblinks[i].getText().equals("Unassign")) {
+				wr = weblinks[i].click();
+				assertTrue(wr.getText().contains("HCP has been unassigned"));
+				break;
+			}
+		}
+	}
+	
+}
---
+++ iTrust/httptests/edu/ncsu/csc/itrust/http/PersonalHealthRecordsUseCaseTest.java
@@ -0,0 +1,121 @@
+package edu.ncsu.csc.itrust.http;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebForm;
+import com.meterware.httpunit.WebResponse;
+
+/**
+ * Use Case 10
+ */
+public class PersonalHealthRecordsUseCaseTest extends iTrustHTTPTest {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		gen.clearAllTables();
+		gen.standardData();
+	}
+
+	public void testEditPatient() throws Exception {
+		// Login
+		WebConversation wc = login("9000000000", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		wr = wr.getLinkWith("PHR Information").click();
+		
+        assertEquals("iTrust - Please Select a Patient", wr.getTitle());
+      
+		WebForm patientForm = wr.getForms()[0];
+		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
+		patientForm.getButtons()[1].click();
+		wr = patientForm.submit();
+		assertTrue(wr.getText().contains("Andy Programmer"));
+	}
+	
+	public void testInvalidPatientDates() throws Exception {
+		// Login
+		WebConversation wc = login("9000000000", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		wr = wr.getLinkWith("Patient Information").click();
+		
+        assertEquals("iTrust - Please Select a Patient", wr.getTitle());
+      
+		WebForm patientForm = wr.getForms()[0];
+		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
+		patientForm.getButtons()[1].click();
+		wr = patientForm.submit();
+		assertTrue(wr.getText().contains("Patient Information"));
+		
+		WebForm editPatientForm = wr.getForms()[0];
+		editPatientForm.getScriptableObject().setParameterValue("dateOfDeathStr", "01/03/2050");
+		wr = editPatientForm.submit();
+		assertTrue(wr.getText().contains("future"));
+		
+	}
+	
+	public void testInvalidPatientBirthDates() throws Exception {
+		// Login
+		WebConversation wc = login("9000000000", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		wr = wr.getLinkWith("Patient Information").click();
+		
+        assertEquals("iTrust - Please Select a Patient", wr.getTitle());
+      
+		WebForm patientForm = wr.getForms()[0];
+		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
+		patientForm.getButtons()[1].click();
+		wr = patientForm.submit();
+		assertTrue(wr.getText().contains("Patient Information"));
+		
+		WebForm editPatientForm = wr.getForms()[0];
+		editPatientForm.getScriptableObject().setParameterValue("dateOfDeathStr", "");
+		editPatientForm.getScriptableObject().setParameterValue("dateOfBirthStr", "01/03/2050");
+		wr = editPatientForm.submit();
+		assertTrue(wr.getText().contains("future"));
+		
+	}
+	
+	
+	public void testRepresent() throws Exception {
+		// Login
+		WebConversation wc = login("9000000000", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		wr = wr.getLinkWith("PHR Information").click();
+		      
+		WebForm patientForm = wr.getForms()[0];
+		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
+		patientForm.getButtons()[1].click();
+		wr = patientForm.submit();
+		
+		wr = wr.getLinkWith("Baby Programmer").click();
+		
+		// Clicking on a representee's name takes you to their records
+		assertTrue(wr.getText().contains("Andy Programmer"));
+		assertTrue(wr.getText().contains("Diabetes with ketoacidosis"));
+		assertTrue(wr.getText().contains("Grandparent"));
+		wr = wr.getLinkWith("Random Person").click();
+		
+		assertTrue(wr.getText().contains("nobody@gmail.com"));
+	}
+
+	public void testAllergy() throws Exception {
+		// Login
+		WebConversation wc = login("9000000000", "pw");
+		WebResponse wr = wc.getCurrentPage();
+		wr = wr.getLinkWith("PHR Information").click();
+		      
+		WebForm patientForm = wr.getForms()[0];
+		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
+		patientForm.getButtons()[1].click();
+		wr = patientForm.submit();
+		
+		// Add allergy
+		WebForm wf = wr.getFormWithName("AddAllergy");
+		wf.getScriptableObject().setParameterValue("description", "Oxygen");
+		wr = wf.submit();
+		
+		wr = wc.getCurrentPage();
+		assertTrue(wr.getText().contains("Allergy Added"));
+		
+		wr = wr.getLinkWith("Show Fake Emails").click();
+		assertTrue(wr.getText().contains("Your medical records have been altered"));
+	}
+}
